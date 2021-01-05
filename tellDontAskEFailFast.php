1e4df3498fef9b383513e59026d9abe7f020acce
Lourdilene Souza <lourdilene.souza@gmail.com>
2021-01-04 21:13:23 -0300

    Aplicação do padrão Tell, don't ask e Fail Fast. Diz oque precisa ser feito

--- tellDontAskEFailFast.php
+++ tellDontAskEFailFast.php
@@ -1,11 +1,17 @@
 <?php
 class PostJaPublicadoException extends \DomainException{}
 class PostStatus{}
-class Post{}
+class Post{
+    public $status = PostStatus::RASCUNHO;
+
+    public function publica(){
+        if ($this->status === PostStatus::RASCUNHO){
+            $this->status = PostStatus::PUBLICADO;
+        }else{
+            throw new PostJaPublicadoException();            
+        }
+    }
+}
 
 $post = new Post();
-if ($post->status === PostSatus::RASCUNHO) {
-    $post->status = PostStatus::PUBLICADO;
-} else {
-    throw new PostJaPublicadoException();    
-}
+$post->publica();
